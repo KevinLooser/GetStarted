b52a1ff18f70353c4f41e984724948abe9c61f34
Kevin Looser <kevin.looser@ict-campus.net>
2018-04-26 13:12:27 +0200

    Staff hinzugefügt

---
+++ Projekt/Aquarium/src/net/ictcampus/aqua/staff/Direktior.java
@@ -0,0 +1,18 @@
+public class Direktor {
+private String name;
+private int yearsOfService;
+public void doThings() {
+System.out.println("I'm working.");
+}
+public int getYearsOfService() {
+return yearsOfService;
+}
+public void setYearsOfService(int yearsOfService) {
+this.yearsOfService = yearsOfService;
+}
+public String getName() { return name;
+}
+public void setName(String name) {
+this.name = name;
+}
+}
